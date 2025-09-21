8fa6eeb357dbbe536d6dc89a2b9d760e16810f0a
KBarts <kbarta@rrc.ca>
2025-09-20 23:35:41 -0500

    feat(routes): add employees routes for branch and department

--- src/api/v1/routes/employees.routes.ts
+++ src/api/v1/routes/employees.routes.ts
@@ -5,9 +5,14 @@ import {
   postEmployee,
   patchEmployee,
   removeEmployee,
+  getEmployeesByBranch,
+  getEmployeesByDepartment,
 } from "../controllers/employees.controller";
 
 export const employeesRouter = Router();
+
+employeesRouter.get("/branch/:branchId", getEmployeesByBranch);
+employeesRouter.get("/department/:department", getEmployeesByDepartment);
 
 employeesRouter.get("/", getAllEmployees);
 employeesRouter.get("/:id", getEmployee);
